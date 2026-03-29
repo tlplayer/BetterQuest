af54e57354f50a4cc60047dc967af83ae25216f4
tplayer <tlplayer@icloud.com>
2026-03-29 08:52:28 -0700

    Add version files

---
+++ Cache.lua
@@ -0,0 +1,38 @@
+
+-------------------------------------------------
+-- MISSING NPC TRACKING (PERSISTENT DB)
+-------------------------------------------------
+
+function Utils:InitializeBetterQuestDB()
+    if not BetterQuestDB then
+        BetterQuestDB = { missingNPCs = {} }
+        Debug("BetterQuestDB initialized")
+    end
+end
+Cache = {}
+
+function Cache:LogMissingNPC(npcName, dialogText, dialogType)
+    if not BetterQuestDB or not npcName or not dialogText then return end
+
+    local normalizedName = npcName
+    local normalizedText = Utils:NormalizeDialogText(dialogText)
+    if normalizedText == "" then return end
+
+
+    if not BetterQuestDB.missingNPCs[normalizedName] then
+        BetterQuestDB.missingNPCs[normalizedName] = {
+            originalName = npcName,
+            dialogs = {},
+            version = Utils:VERSION
+        }
+    end
+
+    local npcEntry = BetterQuestDB.missingNPCs[normalizedName]
+    if not npcEntry.dialogs[normalizedText] then
+        npcEntry.dialogs[normalizedText] = {
+            dialog_text = dialogText,
+            dialogType =  "gossip",
+        }
+    end
+end
+
